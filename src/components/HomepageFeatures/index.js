85cda7ef7091702d0febbc328f2fc8ab14c37041
brendanwenzel <brendanwenzel@gmail.com>
2022-09-29 13:45:06 -0700

    Homepage 70% Done

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,12 +4,12 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Custom Defi Bots',
+    Svg: require('@site/static/img/marketing.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        There are many use cases in Defi to automate your strategies. The most popular
+        is reinvesting staking rewards automatically.
       </>
     ),
   },
@@ -24,8 +24,8 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Digital Marketing',
+    Svg: require('@site/static/img/marketing.svg').default,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
@@ -53,6 +53,7 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
+        <h1 className="text--center padding-horiz--md hero__title">My Superpowers</h1>
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
