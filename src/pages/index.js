353d1ecd50d74af73f6e1a2fd4e2da5f7fd8dec8
brendanwenzel <brendanwenzel@gmail.com>
2022-10-07 14:06:05 -0700

    Added GitHub Button to Hero Section

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            My Story
+            to="https://github.com/brendanwenzel">
+            GitHub
           </Link>
         </div>
       </div>
